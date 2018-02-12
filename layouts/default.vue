c1c5e82edce918870926d11ccaea2a027d9606c7
Jean Yang <bdlas321@gmail.com>
2018-02-12 21:20:08 +0800

    Add: extend default layout

---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+  <main>
+    <header>Home | Materials | Missions | Characters</header>
+    <nuxt/>
+    <footer>
+      <p>All Materials Belong to Koei Tecmo. This site is UNOFFICIAL and just made for personal use.</p>
+      <p><a href="https://jeanycyang.github.io/aot2-guide/">Github</a></p>
+    </footer>
+  </main>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,3 @@
 <template>
-  <h1>Hello world!!</h1>
+  <h1>Attack On Titan 2 - Guide</h1>
 </template>
