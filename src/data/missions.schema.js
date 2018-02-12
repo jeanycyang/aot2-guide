6e4e43b510da81ad05ecec5c91c23abecde77055
Jean Yang <bdlas321@gmail.com>
2018-02-12 17:08:30 +0800

    Add: missions schema

---
+++ src/data/missions.schema.js
@@ -0,0 +1,14 @@
+export default {
+  id: 1,
+  name: 'Inviolable Area',
+  normalModeRewards: {
+    clear: [1], // material ids
+    SRank: [2],
+    dire: [3],
+  },
+  trueAttackModeRewards: {
+    clear: [4],
+    SRank: [5],
+    dire: [6],
+  },
+}
