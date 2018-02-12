4cbacaca9c4292039707bc154097ffaab3ee6b7d
Jean Yang <bdlas321@gmail.com>
2018-02-12 16:43:39 +0800

    Add: materials schema

---
+++ src/data/materials.example.js
@@ -0,0 +1,15 @@
+export const RANKS = {
+  S: 'S',
+  A: 'A',
+  B: 'B',
+  C: 'C',
+  D: 'D',
+}
+
+export default {
+  id: 1,
+  name: "English Name Here",
+  rank: RANKS.D,
+  buyPrice: 81000,
+  sellPrice: 87,
+}
