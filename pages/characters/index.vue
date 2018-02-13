b1734bcce23c5b0643ea32e0a0d4c9894b909ce9
Jean Yang <bdlas321@gmail.com>
2018-02-13 14:38:16 +0800

    Add: filter characters by rank (part 1)

--- pages/characters/index.vue
+++ pages/characters/index.vue
@@ -1,5 +1,20 @@
 <template>
   <div>
+    <div v-if="filter === null">
+      <ul>
+        <li
+          v-for="rank in RANKS"
+          :key="rank"
+          @click="filterByRank(rank)"
+        >
+          {{ rank }}
+        </li>
+      </ul>
+    </div>
+    <div v-else>
+      Characters ranked {{ filter }}
+      <a @click="closeFilter">[x] close filter</a>
+    </div>
     <ul>
       <li
         v-for="(character, key) in CHARACTERS"
@@ -12,11 +27,22 @@
 </template>
 <script>
 import CHARACTERS from '../../src/data/characters'
+import { RANKS } from '../../src/data/characters.schema'
 
 export default {
+  methods: {
+    filterByRank(rank) {
+      this.filter = rank
+    },
+    closeFilter() {
+      this.filter = null
+    },
+  },
   data() {
     return {
+      filter: null,
       CHARACTERS,
+      RANKS,
     }
   },
 }
