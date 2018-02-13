2f6129af2b5d275aee4017995c86f7ce98fdd14a
Jean Yang <bdlas321@gmail.com>
2018-02-13 14:27:23 +0800

    Add: character page

--- components/navbar.vue
+++ components/navbar.vue
@@ -6,7 +6,7 @@
       </li>
       <li class="navbar-item">Missions</li>
       <li class="navbar-item">Materials</li>
-      <li class="navbar-item">Characters</li>
+      <li class="navbar-item"><a href="/characters">Characters</a></li>
       <li class="navbar-item"><a href="/skills">Skills</a></li>
     </ul>
   </header>
---
+++ pages/characters/_name.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>{{ name }} {{ rank }}</h1>
+    <p>Skills: </p>
+  </div>
+</template>
+<script>
+import CHARACTERS from '../../src/data/characters'
+
+export default {
+  validate({ params }) {
+    return !!CHARACTERS[params.name]
+  },
+  data() {
+    const character = CHARACTERS[this.$route.params.name]
+    return { name: character.name, rank: character.rank }
+  },
+  head() {
+    return {
+      title: `${this.name} - Characters - Attack on Titan 2 Guide`,
+      meta: [
+      ],
+    }
+  },
+}
+</script>
