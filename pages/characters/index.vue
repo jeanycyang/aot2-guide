0c0d973611650d66b90cbc708896e3736ff79818
Jean Yang <bdlas321@gmail.com>
2018-02-13 14:22:16 +0800

    Add: characters page

---
+++ pages/characters/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <ul>
+      <li
+        v-for="(character, key) in CHARACTERS"
+        :key="key"
+      >
+        <a :href="'/characters/' + key">{{ character.name }}</a>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+import CHARACTERS from '../../src/data/characters'
+
+export default {
+  data() {
+    return {
+      CHARACTERS,
+    }
+  },
+}
+</script>
