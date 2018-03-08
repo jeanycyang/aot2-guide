554f23b20788341dc9c9fb9ae20a139a4c2f4844
Jean Yang <bdlas321@gmail.com>
2018-03-08 22:02:44 +0800

    Update: remove skills page on navbar

--- components/navbar.vue
+++ components/navbar.vue
@@ -7,7 +7,6 @@
       <li class="navbar-item">Missions</li>
       <li class="navbar-item">Materials</li>
       <li class="navbar-item"><a href="/characters">Characters</a></li>
-      <li class="navbar-item"><a href="/skills">Skills</a></li>
     </ul>
   </header>
 </template>
