4e7bcf3641d58e35dbf8022db9f362b17fe93cbf
Jean Yang <bdlas321@gmail.com>
2018-03-09 21:54:29 +0800

    Add: error report form

--- components/error-report.vue
+++ components/error-report.vue
@@ -1,27 +1,82 @@
 <template>
   <div class="error-report">
-    <div>
-      <p>!</p>
+    <template v-if="show">
+      <div class="form">
+        <form>
+          <input type="text" v-model="title" placeholder="Problem Title" />
+        </form>
+      </div>
+    </template>
+    <div :class="{ 'error-report-button': true, formOpen: show }" @click="toggle">
+      <div>
+        <p v-if="show">✖</p>
+        <p v-else>!</p>
+      </div>
     </div>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      show: false,
+      title: '',
+    }
+  },
+  methods: {
+    toggle() {
+      this.show = !this.show
+    },
+  },
+}
+</script>
 <style scoped>
 .error-report{
   position: fixed;
   bottom: var(--largeSpacing);
   right: var(--largeSpacing);
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+}
+/*
+  postcss doesn't work properly.
+  this variable should only be defined in default.css or other globally injected css file
+*/
+@custom-media --small-viewport (max-width: 768px);
+/* see more: https://github.com/nuxt/nuxt.js/issues/2150 */
+
+@media (--small-viewport) {
+  .error-report{
+    bottom: var(--smallSpacing);
+    right: var(--smallSpacing);
+  }
+}
+.error-report-button{
   border-radius: 100%;
   background: var(--mainGolden);
   box-shadow: 0px 0px 20px 1px rgba(0,0,0,0.5);
   width: 50px;
   height: 50px;
+  cursor: pointer;
 }
-.error-report > div{
+.error-report-button.formOpen{
+}
+.error-report-button > div{
   display: flex;
   justify-content: center;
   align-items: center;
   width: 100%;
   height: 100%;
   font-size: var(--largeFontSize);
+}
+.form{
+  z-index: 999;
+  background: white;
+  height: 500px;
+  width: 300px;
+  max-width: 100%;
+  box-shadow: 0px 0px 20px 1px rgba(0,0,0,0.5);
+  border-radius: 3px;
 }
 </style>
