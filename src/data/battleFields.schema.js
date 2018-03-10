b4d3fa7258a825789fc78ef86c83388a874e4079
Jean Yang <bdlas321@gmail.com>
2018-03-10 11:28:00 +0800

    Add: battleFields & Update: add diffulty to missions schema

---
+++ src/data/battleFields.schema.js
@@ -0,0 +1,5 @@
+export default {
+  id: 1,
+  name: 'SOUTHERN REIGON 1ST SURVEY',
+  missions: [1, 2, 3], // mission ids
+}
--- src/data/missions.schema.js
+++ src/data/missions.schema.js
@@ -1,6 +1,7 @@
 export default {
   id: 1,
   name: 'Inviolable Area',
+  difficulty: 9,
   normalModeRewards: {
     clear: [1], // material ids
     SRank: [2],
